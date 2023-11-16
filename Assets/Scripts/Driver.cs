259677b65bf669fc5c37f7ba217235fa54e237db
GanzaMos <moskalik.ganza@gmail.com>
2023-11-16 17:18:29 +0200

    Repeat and PingPong looping with autolooping

--- Assets/Scripts/Driver.cs
+++ Assets/Scripts/Driver.cs
@@ -1,12 +1,23 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+public enum LoopType
+{
+    Repeat,
+    PingPong
+}
+
 public class Driver : MonoBehaviour
 {
     public Image myImage;
     public float duration;
+    public int loopCount = 1;
+    public LoopType loopType;
+    public bool autoLoop;
+    
     bool _playForward;
     IEnumerator _routine;
 
@@ -37,36 +48,57 @@ public class Driver : MonoBehaviour
             StopCoroutine(_routine);
     }
 
+    float rawResult;
+    
     IEnumerator DriverRoutine()
     {
         float elapsedTime;
+        float wholeDuration = duration * loopCount;
+        
         
         if (_playForward == true)
             elapsedTime = 0;
         else
-            elapsedTime = duration;
+            elapsedTime = wholeDuration;
 
+        //in case if we coroutine was finished in the middle of the progress
         if (_inProgress == true)
             elapsedTime = _progress;
         
-        while (0 <= elapsedTime && elapsedTime <= duration)
+        while (0 <= elapsedTime && elapsedTime <= wholeDuration)
         {
             _inProgress = true;
             
+            //increasing or decreasing value?
             if (_playForward == true)
                 elapsedTime += Time.deltaTime;
             else
                 elapsedTime -= Time.deltaTime;
 
+            //cache the progress Coroutine
             _progress = elapsedTime;
             
-            float result = elapsedTime / duration;
+            rawResult = elapsedTime / duration;
 
+            //at the and of the looping make the value more even
+            if (wholeDuration - rawResult < 0.001)
+                rawResult = loopCount;
+            
+            //now it's time to loop this thing
+            float result = 0;
+            if (loopType == LoopType.Repeat)
+                result = Mathf.Repeat(rawResult, 1.0001f); 
+            else if (loopType == LoopType.PingPong)
+                result = Mathf.PingPong(rawResult, 0.99999f);
+            
             myImage.fillAmount = result;
 
             yield return null;
         }
 
         _inProgress = false;
+
+        if (autoLoop == true)
+            Run(_playForward);
     }
 }
